4a33d8ddab8266aa509fb6c40cafc484f946853f
nimjim <thtetp@gmail.com>
2021-08-19 15:05:42 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# SimpleAuthDNS
+
+## Introduction
+
+SimpleAuthDNS is a simple authoritative DNS server.
+It was made to learn the basics of the DNS protocol and the basic processing flow of the authoritative DNS server.
+Based on this code, it is possible to create an authoritative DNS server with broken or fuzzing response for testing.
+
+## Requirement
+
+- Python 3
+- dnspython (http://www.dnspython.org/)
+
+## Installation and Usage
+
+```bash
+pip install dnspython
+git clone https://github.com/nimjim/SimpleAuthDNS.git
+cd SimpleAuthDNS
+python3 main.py
+```
+
+```bash
+$ python3 main.py -h
+usage: main.py [-h] [-s SERVER] [-p PORT] [-v] [-d] [-f FILE]
+
+This is a simple authoritative dns.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s SERVER, --server SERVER
+                        IP address for listen. Default: all addresses
+  -p PORT, --port PORT  Port for listen. Default: 53
+  -v, --verbose         Print verbose output
+  -d, --debug           Same as "--verbose"
+  -f FILE, --file FILE  The zone file to use. Default: "./test.com.zone"
+```
+
+## Note
+
+The following related implementations have been omitted and are not available.
+
+- queries on qclass other than IN (CH, etc...)
+- wildcard (*.test.com)
+- zone transfer (IXFR, AXFR)
+- transaction authentication (TSIG)
+- CNANE processing
+- DNAME processing
+- DNSSEC processing (DNSKEY, RRSIG, DS, NSEC, NSEC3...)
+- additional options such as ratelimitting, policy actions, view, etc...
+
+The following implementations may be added in the future, but are not available at this time.
+
+- EDNS
+- truncating large response
+
+The following have been implemented and are available.
+
+- query via UDP/TCP
+- basic authoritative DNS processings:
+  - delegation processing (NS)
+  - NXDOMAIN response
+  - NODATA response
+  - simply return the record corresponding to qname and qtype (A, AAAA, TXT, MX, etc...)
+
---
+++ main.py
@@ -0,0 +1,200 @@
+#! /usr/bin/env python3
+
+import argparse
+import socket
+import select
+import threading
+
+import dns.zone
+import dns.name
+import dns.message
+import dns.rcode
+import dns.rdatatype
+import dns.rdataclass
+import dns.query
+
+SERVER = ''  # for all addresses
+PORT = 53
+DEBUG_MODE = False
+zonefile = './test.com.zone'
+zone = dns.zone.from_file(zonefile, relativize=False)
+
+def process_command_line_args():
+    global SERVER, PORT, DEBUG_MODE, zone
+
+    parser = argparse.ArgumentParser(
+        description='This is a simple authoritative dns.')
+    parser.add_argument('-s', '--server',
+                        help='IP address for listen. Default: all addresses')
+    parser.add_argument('-p', '--port',
+                        help='Port for listen. Default: 53')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Print verbose output')
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='Same as "--verbose"')
+    parser.add_argument('-f', '--file',
+                        help='The zone file to use. Default: "./test.com.zone"')
+    args = parser.parse_args()
+
+    if args.server:
+        SERVER = args.server
+    if args.port:
+        PORT = int(args.port)
+    if args.debug or args.verbose:
+        DEBUG_MODE = True
+        print('running in DEBUG MODE')
+    if args.file:
+        zone = dns.zone.from_file(args.file, relativize=False)
+
+def setup_sockets(server, port):
+    sock_udp4 = create_udp4_socket(server, port)
+    sock_udp6 = create_udp6_socket(server, port)
+    sock_tcp4 = create_tcp4_socket(server, port)
+    sock_tcp6 = create_tcp6_socket(server, port)
+
+    fd_udp4 = sock_udp4.fileno()
+    fd_udp6 = sock_udp6.fileno()
+    fd_tcp4 = sock_tcp4.fileno()
+    fd_tcp6 = sock_tcp6.fileno()
+
+    fd_read = [fd_udp4, fd_udp6, fd_tcp4, fd_tcp6]
+    sockets = {fd_udp4: sock_udp4, fd_udp6: sock_udp6,
+               fd_tcp4: sock_tcp4, fd_tcp6: sock_tcp6}
+    is_tcp_dict = {fd_udp4: False, fd_udp6: False, fd_tcp4: True, fd_tcp6: True}
+
+    return fd_read, sockets, is_tcp_dict
+
+def create_udp4_socket(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((host, port))
+    return sock
+
+def create_udp6_socket(host, port):
+    sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 1)
+    sock.bind((host, port))
+    return sock
+
+def create_tcp4_socket(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((host, port))
+    sock.listen(5)
+    return sock
+
+def create_tcp6_socket(host, port):
+    sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((host, port))
+    sock.listen(5)
+    return sock
+
+def handle_connection(sock, is_tcp):
+    if is_tcp:
+        conn, client_addrport = sock.accept()
+        query, received_time = dns.query.receive_tcp(conn)
+    else:
+        query, received_time, client_addrport = dns.query.receive_udp(sock)
+
+    if DEBUG_MODE:
+        transport = "TCP" if is_tcp else "UDP"
+        qname = query.question[0].name
+        qtype = dns.rdatatype.to_text(query.question[0].rdtype)
+        qclass = dns.rdataclass.to_text(query.question[0].rdclass)
+        addr, port = client_addrport
+        print('\n{} query: {} {} {} from {}#{}, id: {}'.format(
+            transport, qname, qtype, qclass, addr, port, query.id))
+
+    response = resolve_query(query)
+
+    if DEBUG_MODE:
+        print("Response for", response)
+
+    # Response the answer. If response is None, it means query is dropped.
+    if response is not None:
+        if is_tcp:
+            dns.query.send_tcp(conn, response)
+            conn.close()
+        else:
+            dns.query.send_udp(sock, response, client_addrport)
+
+def resolve_query(query):
+    response = dns.message.make_response(query)
+    qname = query.question[0].name
+    qtype = query.question[0].rdtype
+    qclass = query.question[0].rdclass
+
+    # Refuse query if qclass is not IN
+    if qclass != dns.rdataclass.IN:
+        response.set_rcode(dns.rcode.REFUSED)
+        return response
+
+    # Refuse query for a different Zone
+    if not qname.is_subdomain(zone.origin):
+        response.set_rcode(dns.rcode.REFUSED)
+        return response
+
+    # Return NOTIMP for RR Types which SimpleAuthoritativeDNS does not support
+    # https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml#dns-parameters-4
+    if qtype >= 99:
+        response.set_rcode(dns.rcode.NOTIMP)
+        return response
+
+    # Check for delegation
+    for i in range(len(qname.labels) - len(zone.origin.labels)):
+        check_name = dns.name.Name(labels=qname.labels[i:])
+        ns_rdataset = zone.get_rdataset(check_name, dns.rdatatype.NS)
+        if ns_rdataset:
+            # do referral response
+            response.authority.append(
+                zone.get_rrset(check_name, dns.rdatatype.NS))
+            for rdata in ns_rdataset:
+                # check if ns target is in-Bailiwick or out-of-Bailiwick
+                ns_target = rdata.target
+                if ns_target.is_subdomain(check_name):
+                    # if ns target is in-Bailiwick, add glue record
+                    for rrtype in (dns.rdatatype.A, dns.rdatatype.AAAA):
+                        rrset = zone.get_rrset(ns_target, rrtype)
+                        if rrset:
+                            response.additional.append(rrset)
+            return response
+
+    # Look for qname
+    node = zone.get_node(qname)
+    if node is None:
+        # NXDOMAIN response
+        response.set_rcode(dns.rcode.NXDOMAIN)
+        soa_rrset = zone.get_rrset(zone.origin, dns.rdatatype.SOA)
+        response.authority.append(soa_rrset)
+        response.flags |= dns.flags.AA
+        return response
+
+    # Look for (qname, qtype)
+    rrset = zone.get_rrset(qname, qtype)
+    if rrset is None:
+        # NODATA response
+        response.set_rcode(dns.rcode.NOERROR)
+        soa_rrset = zone.get_rrset(zone.origin, dns.rdatatype.SOA)
+        response.authority.append(soa_rrset)
+        response.flags |= dns.flags.AA
+    else:
+        response.set_rcode(dns.rcode.NOERROR)
+        response.answer.append(rrset)
+        response.flags |= dns.flags.AA
+    return response
+
+def main():
+    process_command_line_args()
+    print("SimpleAuthDNS: running")
+    fd_read, sockets, is_tcp_dict = setup_sockets(SERVER, PORT)
+    print("Listening on UDP and TCP port {}".format(PORT))
+
+    while True:
+        fd_read_ready, _, _ = select.select(fd_read, [], [], 5)
+        for fd in fd_read_ready:
+            threading.Thread(target=handle_connection,
+                             args=(sockets[fd], is_tcp_dict[fd])).start()
+
+if __name__ == '__main__':
+    main()
